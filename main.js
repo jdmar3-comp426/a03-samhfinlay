99eaa41be5fd0ba10eebcb6f403e74ac94c98dfa
Sam Finlay <69521209+samhfinlay@users.noreply.github.com>
2021-11-10 11:17:11 -0500

    jh

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 import { sumToString, getIncreasingArray, maxAndMin, countArray } from "./src/mild/mild_1.js";
 import { identifyVariable, identifyArray, removeKey } from "./src/mild/mild_2.js";
-let obj = {
-    name: 'Mr. Boss',
-    title: 'boss',
-    age: 33,
-    password: 'pass123'
-};
-removeKey(obj, 'password');
-console.log(obj);
+console.log(identifyArray(['baby', 3, 'ad', [1, 2]]));
--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -68,7 +68,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+obj {key, ...rest};
 }
 
 /**
